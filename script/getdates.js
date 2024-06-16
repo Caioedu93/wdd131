f02b4c56b7cb90d32cc31706f6671679167f4060
Caioededu93 <depaulacaio10@gmail.com>
2024-06-16 00:50:27 -0300

    mypage

---
+++ script/getdates.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const currentYear = new Date().getFullYear();
+
+    const lastModified = document.lastModified;
+
+
+    const copyrightYearElement = document.getElementById("copyright-year");
+    if (copyrightYearElement) {
+        copyrightYearElement.textContent = `© ${currentYear}  Caio Eduardo Jesus de Paula  Brasil`;
+    }
+
+
+
+    const lastModifiedElement = document.getElementById("lastModified");
+    if (lastModifiedElement) {
+        lastModifiedElement.textContent = `Last Modified: ${lastModified}  `;
+    }
+});
