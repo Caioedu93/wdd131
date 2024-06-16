6bb8a8db361a2dce4dc43485dbd4f8b89123c417
Caioededu93 <depaulacaio10@gmail.com>
2024-06-16 00:53:09 -0300

    update

--- script/getdates.js
+++ script/getdates.js
@@ -6,13 +6,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const copyrightYearElement = document.getElementById("copyright-year");
     if (copyrightYearElement) {
-        copyrightYearElement.textContent = `© ${currentYear}  Caio Eduardo Jesus de Paula  Brasil`;
+        copyrightYearElement.textContent = `© ${currentYear} ⚽  Caio Eduardo Jesus de Paula  Brasil  ⚽`;
     }
 
 
 
     const lastModifiedElement = document.getElementById("lastModified");
     if (lastModifiedElement) {
-        lastModifiedElement.textContent = `Last Modified: ${lastModified}  `;
+        lastModifiedElement.textContent = `Last Modified: ${lastModified}`;
     }
 });
