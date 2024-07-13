ab2130c96b8364c062441ca937f575fd8dbb6247
Caioededu93 <depaulacaio10@gmail.com>
2024-07-13 13:30:54 -0300

    forms

---
+++ script/forms.js
@@ -0,0 +1,18 @@
+const products = [
+    { id: "fc-1888", name: "flux capacitor" },
+    { id: "fc-2050", name: "power laces" },
+    { id: "fs-1987", name: "time circuits" },
+    { id: "ac-2000", name: "low voltage reactor" },
+    { id: "jj-1969", name: "warp equalizer" }
+  ];
+  
+  window.onload = function() {
+    const productNameSelect = document.getElementById("productName");
+    products.forEach(product => {
+      const option = document.createElement("option");
+      option.value = product.id;
+      option.textContent = product.name;
+      productNameSelect.appendChild(option);
+    });
+  }
+  
---
+++ script/review.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    let reviewCount = localStorage.getItem("reviewCount");
+    if (!reviewCount) {
+      reviewCount = 0;
+    }
+    reviewCount++;
+    localStorage.setItem("reviewCount", reviewCount);
+    document.getElementById("reviewCount").textContent = reviewCount;
+  }
+  
